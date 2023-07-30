dad8356e98970518d212866e25a4b563e4cea17a
chideraemmanuel <120053997+chideraemmanuel@users.noreply.github.com>
2023-07-30 08:06:54 +0100

    changes movie card styles

--- src/components/movieCard/MovieCard.tsx
+++ src/components/movieCard/MovieCard.tsx
@@ -1,6 +1,6 @@
 import "./MovieCard.scss";
 import { AiOutlinePlus } from "react-icons/ai";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { carouselCardPropTypes } from "../../utilities/componentsTypes";
 import getPosterUrl from "../../utilities/getPosterUrl";
 import Rating from "../rating/Rating";
@@ -8,12 +8,17 @@ import Rating from "../rating/Rating";
 const MovieCard = ({ data: movie }: carouselCardPropTypes) => {
   const posterUrl = getPosterUrl(movie.poster_path);
 
+  const navigate = useNavigate();
+
   return (
     <div
       className="movie-card"
       style={{ background: `url(${posterUrl}) no-repeat center center/cover` }}
     >
-      <div className="movie-card__overlay"></div>
+      <div
+        className="movie-card__overlay"
+        onClick={() => navigate(`/movie/${movie.id}`)}
+      ></div>
 
       <div className="movie-card__info">
         <Link to={`/movie/${movie.id}`} className="movie-card__info--title">
